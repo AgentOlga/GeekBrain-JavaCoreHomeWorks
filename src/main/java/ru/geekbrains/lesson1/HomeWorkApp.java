987fd0b252ec204efa3b4f203a9f5625dc0b7e55
AgentOlga <agentolga@gmail.com>
2022-02-12 14:44:06 +0100

    --

--- src/main/java/ru/geekbrains/lesson1/HomeWorkApp.java
+++ src/main/java/ru/geekbrains/lesson1/HomeWorkApp.java
@@ -8,15 +8,20 @@ public class HomeWorkApp
         checkSumSign();
         printColor();
         compareNumbers();
+        System.out.println("method2_1 returns: " + method2_1(8, 3));
+        method2_2(-10);
+        System.out.println("method2_3 returns: " + method2_3(23));
+        method2_4( "I have fun",9);
+
     }
-    public static void printThreeWords()
+    static void printThreeWords()
     {
         System.out.println("Orange");
         System.out.println("Apple");
         System.out.println("Banana");
     }
 
-    public static void checkSumSign()
+    static void checkSumSign()
     {
         int tasksCount1 = 6;
         int tasksCount2 = 2;
@@ -30,7 +35,7 @@ public class HomeWorkApp
         }
 
     }
-    public static void printColor()
+    static void printColor()
     {
         int value = 1;
         if (value <= 0) {
@@ -44,7 +49,7 @@ public class HomeWorkApp
         }
 
     }
-    public static void compareNumbers()
+    static void compareNumbers()
     {
         int a = 10;
         int b = 4;
@@ -56,5 +61,25 @@ public class HomeWorkApp
         }
     }
 
+    static boolean method2_1(int param1, int param2) {
+        int sum = param1 + param2;
+        return sum >= 10 && sum <= 20;
+    }
+
+    static void method2_2(int param) {
+        System.out.println(param >= 0 ? "Positive" : "Negative");
+    }
+
+    static boolean method2_3(int param) {
+        return param < 0;
+        }
+
+
+
+    public static void method2_4(String word, int times) {
+        for ( int i = 0; i < times; i++){
+            System.out.println(word);
+        }
+    }
 }
 
