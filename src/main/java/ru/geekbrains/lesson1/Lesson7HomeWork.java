7f847453d611f260693bf292a5de0c3986aac57e
AgentOlga <agentolga@gmail.com>
2022-03-02 21:08:04 +0100

    add Lesson7HomeWork.java

--- src/main/java/ru/geekbrains/lesson1/Lesson7HomeWork.java
+++ src/main/java/ru/geekbrains/lesson1/Lesson7HomeWork.java
@@ -8,22 +8,20 @@ package ru.geekbrains.lesson1;
 public class Lesson7HomeWork {
     public static void main(String[] args) {
 
-        int action;
         Cat[] allCats = new Cat[4];
         allCats[0] = new Cat("Barsik", 5, false);
         allCats[1] = new Cat("Snow", 30, false);
         allCats[2] = new Cat("Murzik", 10, false);
         allCats[3] = new Cat("Blacky", 45, false);
 
-        Plate plate = new Plate(15);
+        Plate plate = new Plate(50);
         plate.info();
         for (int i = 0; i < allCats.length; i++) {
-            if (allCats[i].fullness == false && allCats[i].appetite < plate.food) {
-                allCats[i].eat (plate);
+            if (allCats[i].fullness == false && allCats[i].appetite < plate.getFood()) {
+                allCats[i].eat(plate);
                 allCats[i].fullness = true;
                 System.out.println("Cat " + allCats[i].name + " ate!");
-            } else
-            {
+            } else {
                 System.out.println("Cat " + allCats[i].name + " didn't eat!");
             }
         }
@@ -31,6 +29,15 @@ public class Lesson7HomeWork {
         System.out.println("How many grams of food did you add to the bowl?");
         plate.increaseFood(10);
         plate.info();
+
+        do {
+            for (Cat cat : allCats) {
+                cat.eat(plate);
+            }
+        } while (!plate.empty());
+        for (Cat cat : allCats) {
+            cat.info();
+        }
     }
 }
 
@@ -39,33 +46,65 @@ class Cat {
     public int appetite;
     public boolean fullness;
 
+    String getName() {
+        return name;
+    }
+    int getAppetite() {
+        return appetite;
+    }
+
+
     public Cat(String name, int appetite, boolean fullness) {
         this.name = name;
         this.appetite = appetite;
         this.fullness = fullness;
     }
 
-    public void eat(Plate p ){
-        p.decreaseFood(appetite);
+    public void eat(Plate plate){
+        int food = plate.getFood();
+        plate.decreaseFood(appetite);
+        if (food > this.appetite)
+            appetite = 0;
+        else
+            appetite -= food;
+    }
 
+    public void info() {
+        if (fullness)
+            System.out.println("cat " + name + " is full.");
+        else
+            System.out.println("cat " + name + " is " + appetite + " hungry.");
     }
 }
 
 class Plate {
-    public int food;
+    private int food;
+
+    int getFood() {
+        return food;
+    }
+
+    boolean empty() {
+        return food == 0;
+    }
 
     public Plate (int food) {
         this.food = food;
     }
 
     public void increaseFood(int food) {
+        this.food += food;
+    }
+
+    public void decreaseFood(int food) {
+        if (food > this.food)
+            this.food = 0;
+        else
+            this.food = this.food - food;
     }
 
     public void info() {
         System.out.println("plate: " + food);
-    }
-
-    public void decreaseFood(int appetite) {
     }
 }
 
